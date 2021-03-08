3b1960a0b3fad418ff8f3858f538a2a26cf49627
SeaSnake10 <76809064+SeaSnake10@users.noreply.github.com>
2021-03-08 08:55:11 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock, dImg;
+
+
+
+function preload()
+{
+	//load images here
+  dImg = loadImage("images/dogImg.png");
+  happyDog = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog = createSprite(250,300,100,10);
+  dog.addImage(dImg);
+  dog.scale = 0.15;
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock)
+}
+
+
+function draw() {  
+  background(46, 139, 87)
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS)
+    dog.addImage(happyDog);
+  }
+  drawSprites();
+  //add styles here
+  textSize(15);
+  fill("black");
+  text("Score:" + foodStock, 250, 50);
+}
+
+function readStock(data){
+  foodS=data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
